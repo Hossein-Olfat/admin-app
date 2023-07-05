1cc42dd1322a345cf979c851f946f95713710fa7
Hossein-Olfat <h1382olfat@gmail.com>
2023-07-05 01:22:22 -0700

    init chart and chart_data for Home_content

--- my-project/src/Components/Home_content/Home_content.jsx
+++ my-project/src/Components/Home_content/Home_content.jsx
@@ -1,8 +1,11 @@
 import { Profit_summary } from "./Profit_summary/Profit_summary.jsx";
+import Chart from "../Chart/Chart";
+import { xAxisdata } from "../../Chart_datas";
 function Home_Content() {
   return (
     <div className=" flex flex-col">
       <Profit_summary />
+      <Chart grid title="Month Sale" data={xAxisdata} datakey="Sale" />
     </div>
   );
 }
